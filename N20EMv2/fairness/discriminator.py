05c4203fc03250466c7ca6a94de5174506bda6e4
guxm2021 <guxm2021@gmail.com>
2023-03-10 13:56:31 +0800

    upload fairness

---
+++ N20EMv2/fairness/discriminator.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+class DiscNet(nn.Module):
+    """
+    Discriminator doing binary classification: source v.s. target
+    """
+    def __init__(self, input_dim, hidden_dim):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc5 = nn.Linear(hidden_dim, 1)
+    
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
+        x = torch.relu(self.fc5(x))
+        return x
